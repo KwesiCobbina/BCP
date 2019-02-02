310b334cf1abaaf06b359b8393e61add02bc38c4
Kwesi Cobbina <kwecobi@gmail.com>
2019-02-02 08:23:55 +0000

    Initial Commit

---
+++ BCP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BCP
+//
+//  Created by Kwesi Adu Cobbina on 02/02/2019.
+//  Copyright © 2019 Kwesi Adu Cobbina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
